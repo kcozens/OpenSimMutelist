a38e17e11fa4bc31399e6789224ebb2c82c6e5dd
Kevin Cozens <kevin@ve3syb.ca>
2017-12-04 12:07:06 -0500

    Use the MyISAM engine for the mutelist table

--- webroot/sql/mutelist.sql
+++ webroot/sql/mutelist.sql
@@ -3,10 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 22, 2017 at 11:12 PM
+-- Generation Time: Dec 04, 2017 at 11:54 AM
 -- Server version: 5.7.20
 -- PHP Version: 7.0.22-0ubuntu0.16.04.1
-
 
 --
 -- Database: `osmodules`
@@ -24,16 +23,7 @@ CREATE TABLE IF NOT EXISTS `mutelist` (
   `MuteName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `type` int(11) UNSIGNED NOT NULL,
   `flags` int(11) UNSIGNED NOT NULL,
-  `Stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `mutelist`
---
-ALTER TABLE `mutelist`
-  ADD UNIQUE KEY `AgentID_2` (`AgentID`,`MuteID`) USING BTREE,
-  ADD KEY `AgentID` (`AgentID`) USING BTREE;
+  `Stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `AgentID_2` (`AgentID`,`MuteID`) USING BTREE,
+  KEY `AgentID` (`AgentID`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
